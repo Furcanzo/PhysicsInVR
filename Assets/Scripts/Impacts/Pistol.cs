df48d0fa52198732f5c086a5cb3581d7eb915575
Furcanzo <roberto.buratti@mail.polimi.it>
2022-09-11 18:08:40 +0200

    summer job

--- Assets/Scripts/Impacts/Pistol.cs
+++ Assets/Scripts/Impacts/Pistol.cs
@@ -6,9 +6,10 @@ namespace Impacts
     {
         public float massOfCannonball = 2;
         public GameObject vectorModel;
-        public float cannonballVectorScaleFactor = 2.0f;
+        public float cannonballVectorScaleFactor = 15.0f;
         public float cannonballScaling = 0.05f;
         public float startForce = 100.0f;
+        public bool elastic;
         public GameObject Aim;
         
         private float _cannonballDimension;
@@ -19,7 +20,15 @@ namespace Impacts
         public void Shoot()
         {
             _cannonball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            PhysicMaterial cannonballMaterial = new PhysicMaterial
+            {
+                staticFriction = 0.0f,
+                dynamicFriction = 0.0f,
+                bounciness = elastic? 1.0f : 0.0f,
+                bounceCombine = PhysicMaterialCombine.Multiply
+            };
             _cannonball.AddComponent<Rigidbody>().mass = massOfCannonball;
+            _cannonball.GetComponent<Collider>().material = cannonballMaterial;
             _rb = _cannonball.GetComponent<Rigidbody>();
             _rb.useGravity = false;
             _cannonballDimension = _rb.mass * cannonballScaling;
